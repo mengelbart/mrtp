9ba0dc2fed6286a06f4dfdfcd5bdeff00fd87f78
Fabian Fromwald <fabian@fromwald.at>
2025-11-12 18:22:47 +0100

    Add more gstreamer probes to track frames across pipeline

--- gstreamer/pad_probe.go
+++ gstreamer/pad_probe.go
@@ -1,6 +1,8 @@
 package gstreamer
 
 import (
+	"log/slog"
+
 	"github.com/go-gst/go-gst/gst"
 	"github.com/mengelbart/mrtp/logging"
 	"github.com/pion/rtp"
@@ -37,6 +39,67 @@ func getRTPLogPadProbe(vantagePointName string) func(p *gst.Pad, ppi *gst.PadPro
 				}
 				logger.LogRTPPacket(&b.Header, b.Payload, nil)
 			}
+		}
+		return gst.PadProbeOK
+	}
+}
+
+func getRTPtoPTSMappingProbe(eventName string) func(p *gst.Pad, ppi *gst.PadProbeInfo) gst.PadProbeReturn {
+	return func(pad *gst.Pad, info *gst.PadProbeInfo) gst.PadProbeReturn {
+		buffer := info.GetBuffer()
+		if buffer != nil {
+			// Get PTS from buffer metadata
+			pts := buffer.PresentationTimestamp().AsDuration()
+			offset := buffer.Offset()
+
+			var ptsMs int64
+			if pts != nil {
+				ptsMs = pts.Milliseconds()
+			}
+
+			// Parse RTP packet to get RTP timestamp
+			mapinfo := buffer.Map(gst.MapRead)
+			defer buffer.Unmap()
+			pkt := mapinfo.AsUint8Slice()
+
+			if len(pkt) >= 12 {
+				// Extract RTP timestamp
+				rtpTimestamp := uint32(pkt[4])<<24 | uint32(pkt[5])<<16 | uint32(pkt[6])<<8 | uint32(pkt[7])
+
+				slog.Info(eventName,
+					"rtp-timestamp", rtpTimestamp,
+					"pts", ptsMs,
+					"offset", offset)
+			}
+		}
+		return gst.PadProbeOK
+	}
+}
+
+func getFrameProbe(eventName string) func(p *gst.Pad, ppi *gst.PadProbeInfo) gst.PadProbeReturn {
+	frameCount := uint64(0)
+	return func(p *gst.Pad, ppi *gst.PadProbeInfo) gst.PadProbeReturn {
+		buffer := ppi.GetBuffer()
+		if buffer != nil {
+			pts := buffer.PresentationTimestamp().AsDuration()
+			dts := buffer.DecodingTimestamp().AsDuration()
+			duration := buffer.Duration().AsDuration()
+			offset := buffer.Offset()
+
+			// Convert to milliseconds
+			var ptsMs, dtsMs, durationMs int64
+			if pts != nil {
+				ptsMs = pts.Milliseconds()
+			}
+			if dts != nil {
+				dtsMs = dts.Milliseconds()
+			}
+			if duration != nil {
+				durationMs = duration.Milliseconds()
+			}
+
+			slog.Info(eventName, "pts", ptsMs, "dts", dtsMs, "duration", durationMs, "offset", offset, "frame-count", frameCount)
+			frameCount++
 		}
 		return gst.PadProbeOK
 	}
