5059882340ed8193adf5fde2c9a41c722ec48ed2
Fabian Fromwald <fabian@fromwald.at>
2026-01-14 18:59:04 +0100

    Fix webrtc dc usage and log data events correctly

--- data/sink.go
+++ data/sink.go
@@ -3,6 +3,8 @@ package data
 import (
 	"io"
 	"log/slog"
+
+	"github.com/quic-go/quic-go/quicvarint"
 )
 
 // DataSink currently only a noop sink
@@ -20,9 +22,13 @@ func NewSink(rc io.ReadCloser) (*DataSink, error) {
 
 func (d *DataSink) Run() error {
 	slog.Info("DataSink started")
-	buf := make([]byte, 1024)
+
+	currentChunk := 0
+
 	for {
-		n, err := d.rc.Read(buf)
+		// Read chunk size header first
+		headerBuf := make([]byte, 10)
+		n, err := d.rc.Read(headerBuf)
 		if err != nil {
 			if err == io.EOF {
 				slog.Info("DataSink finished")
@@ -32,6 +38,50 @@ func (d *DataSink) Run() error {
 			return err
 		}
 
-		slog.Info("DataSink received data", "payload-length", n)
+		chunkSize, varintLen, err := quicvarint.Parse(headerBuf[:n])
+		if err != nil {
+			return err
+		}
+
+		slog.Info("DataSink Chunk started", "chunk-number", currentChunk, "chunk-size", chunkSize)
+
+		if chunkSize == 0 { // only one chunk
+			slog.Info("DataSink chunksize 0")
+			buf := make([]byte, 2048)
+			for {
+				n, err := d.rc.Read(buf)
+				if err != nil {
+					if err == io.EOF {
+						slog.Info("DataSink Chunk finished", "chunk-number", currentChunk)
+						return nil
+					}
+					slog.Info("Datasink error: ", "err", err)
+					return err
+				}
+
+				slog.Info("DataSink received data", "payload-length", n, "chunk-number", currentChunk)
+			}
+		} else {
+			remainingFromHeader := n - varintLen
+			read := remainingFromHeader
+
+			buf := make([]byte, 2048)
+			for read < int(chunkSize) {
+				n, err := d.rc.Read(buf)
+				if err != nil {
+					if err == io.EOF {
+						slog.Info("DataSink Chunk finished", "chunk-number", currentChunk)
+						return nil
+					}
+					slog.Info("Datasink error: ", "err", err)
+					return err
+				}
+
+				read += n
+				slog.Info("DataSink received data", "payload-length", n, "chunk-number", currentChunk)
+			}
+			slog.Info("DataSink Chunk finished", "chunk-number", currentChunk)
+			currentChunk++
+		}
 	}
 }
