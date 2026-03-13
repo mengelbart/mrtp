2ecce173a520ae767da874ae76a87d74e9082770
Fabian Fromwald <fabian@fromwald.at>
2026-03-13 10:47:00 +0100

    Concatenate simulation test video to extend duration

--- simulation/get-video.sh
+++ simulation/get-video.sh
@@ -2,6 +2,13 @@
 
 if [ ! -f "Johnny_1280x720_60.y4m" ]; then
     wget https://media.xiph.org/video/derf/y4m/Johnny_1280x720_60.y4m
+    concat_file=$(mktemp /tmp/concat_XXXXXX.txt)
+    for i in $(seq 1 10); do
+        echo "file '$(realpath Johnny_1280x720_60.y4m)'" >> "$concat_file"
+    done
+    ffmpeg -f concat -safe 0 -i "$concat_file" -c copy Johnny_1280x720_60_tmp.y4m
+    rm "$concat_file"
+    mv Johnny_1280x720_60_tmp.y4m Johnny_1280x720_60.y4m
 else
     echo "Johnny_1280x720_60.y4m already exists."
 fi
