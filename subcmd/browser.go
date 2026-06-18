1e18adddcc823cbb33d3e685445f183e5602c080
Fabian Fromwald <fabian@fromwald.at>
2026-06-18 17:39:48 +0200

    Add datachannel that sends random data

--- subcmd/browser.go
+++ subcmd/browser.go
@@ -20,27 +20,30 @@ type browserSubCmd struct {
 	sourceLocation string
 	localPort      string
 	remotePort     string
+
+	datachannel  bool
+	dcStartDelay uint
 }
 
 // Exec implements cmdmain.SubCmd.
 func (b *browserSubCmd) Exec(cmd string, args []string) error {
 	fs := flag.NewFlagSet("browser", flag.ExitOnError)
 	fs.StringVar(&b.sourceLocation, "source-location", "", "Location for filesource")
-
 	fs.StringVar(&b.localAddr, "local", "127.0.0.1", "Local address")
 	fs.StringVar(&b.remoteAddr, "remote", "127.0.0.1", "Remote address")
-
 	fs.StringVar(&b.localPort, "local-port", "8080", "Local port of HTTP signaling server to listen on")
 	fs.StringVar(&b.remotePort, "remote-port", "8080", "Remote Port of HTTP signaling server to connect to")
+	fs.BoolVar(&b.datachannel, "dc", false, "Send/Receive data with data channels")
+	fs.UintVar(&b.dcStartDelay, "dc-start-delay", 0, "Start delay in seconds before data channel source starts sending data.")
 
 	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, `Run a sender
+		fmt.Fprintf(os.Stderr, `%s
 
 Usage:
 	%s browser [flags]
 
 Flags:
-`, cmd)
+`, b.Help(), cmd)
 		fs.PrintDefaults()
 		fmt.Fprintln(os.Stderr)
 	}
@@ -49,7 +52,7 @@ Flags:
 	}
 
 	if _, err := os.Stat(b.sourceLocation); os.IsNotExist(err) {
-		fmt.Printf("Cannot find video file: %v\n", b.sourceLocation)
+		fmt.Fprintf(os.Stderr, "Cannot find video file: %v\n", b.sourceLocation)
 		fs.Usage()
 		os.Exit(1)
 	}
@@ -57,16 +60,25 @@ Flags:
 	var err error
 	b.sourceLocation, err = filepath.Abs(b.sourceLocation)
 	if err != nil {
-		fmt.Printf("Error with video: %v\n", b.sourceLocation)
+		fmt.Fprintf(os.Stderr, "Error with video: %v\n", b.sourceLocation)
 		fs.Usage()
 		os.Exit(1)
 	}
 
-	ctrl := browser.NewController(b.sourceLocation, b.localAddr, b.localPort, b.remoteAddr, b.remotePort)
+	opts := []browser.Option{}
+	if b.datachannel {
+		opts = append(opts, browser.UseDatachannel(b.dcStartDelay))
+	}
+
+	ctrl, err := browser.NewController(b.sourceLocation, b.localAddr, b.localPort, b.remoteAddr, b.remotePort, opts...)
+	if err != nil {
+		return err
+	}
+
 	return ctrl.Run()
 }
 
 // Help implements cmdmain.SubCmd.
 func (b *browserSubCmd) Help() string {
-	return "Run a remote controlled browser"
+	return "Run a remote controlled browser. Sends a video via WebRTC. Can connect to the webrtc subcmd"
 }
