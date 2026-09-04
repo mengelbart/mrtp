bc7541eaca37a3fd50a36bea3c4acfabe158280f
Mathis Engelbart <mathis.engelbart@gmail.com>
2026-09-04 20:53:52 +0200

    Remove unused code

--- internal/web/handler.go
+++ internal/web/handler.go
@@ -16,40 +16,19 @@ import (
 //go:embed templates/*
 var templateFS embed.FS
 
-type Option func(*Handler) error
-
-func Mux(mux *httprouter.Router) Option {
-	return func(h *Handler) error {
-		h.mux = mux
-		return nil
-	}
-}
-
-type Handler struct {
-	mux *httprouter.Router
-}
-
-func NewHandler(opts ...Option) (*Handler, error) {
-	s := &Handler{
-		mux: httprouter.New(),
-	}
-	for _, opt := range opts {
-		if err := opt(s); err != nil {
-			return nil, err
-		}
-	}
-
+// Register mounts the frontend pages and their static assets on mux.
+func Register(mux *httprouter.Router) error {
 	manifestData, err := frontendFS.ReadFile("frontend/dist/.vite/manifest.json")
 	if err != nil {
-		return nil, err
+		return err
 	}
 	var m manifest
 	if err = m.parse(manifestData); err != nil {
-		return nil, err
+		return err
 	}
 	templateFiles, err := templateFS.ReadDir("templates")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	pages := map[string]*template.Template{}
@@ -59,41 +38,35 @@ func NewHandler(opts ...Option) (*Handler, error) {
 		var tmplFile []byte
 		tmplFile, err = templateFS.ReadFile(fmt.Sprintf("templates/%v", f.Name()))
 		if err != nil {
-			return nil, err
+			return err
 		}
 		var t *template.Template
 		t, err = template.New(page).Parse(string(tmplFile))
 		if err != nil {
-			return nil, err
+			return err
 		}
 		pages[page] = t
 	}
-	s.mux.HandlerFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandlerFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/index", http.StatusFound)
 	})
 
 	for page, tmpl := range pages {
 		slog.Info("setting up handler to render page", "page", page)
-		s.mux.HandlerFunc("GET", fmt.Sprintf("/%v", page), s.renderHTML(page, m, tmpl))
+		mux.HandlerFunc("GET", fmt.Sprintf("/%v", page), renderHTML(page, m, tmpl))
 	}
 
 	publicFS, err := fs.Sub(frontendPublicFS, "frontend/dist")
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// Uncomment for net/http.ServeMux version
-	// s.mux.Handler("GET", "/static/", http.StripPrefix("/static/", http.FileServer(http.FS(publicFS))))
-	s.mux.ServeFiles("/static/*filepath", http.FS(publicFS))
-	return s, nil
+	mux.ServeFiles("/static/*filepath", http.FS(publicFS))
+	return nil
 }
 
-func (h *Handler) renderHTML(name string, m manifest, t *template.Template) http.HandlerFunc {
-	type templateData struct {
-		Chunk          chunk
-		ImportedChunks []chunk
-	}
+func renderHTML(name string, m manifest, t *template.Template) http.HandlerFunc {
 	name = fmt.Sprintf("src/%s.ts", name)
-	data := templateData{
+	data := viteTemplateData{
 		Chunk:          m[name],
 		ImportedChunks: m.importedChunks(name),
 	}
